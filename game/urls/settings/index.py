83f1cae077f952fcc4cf5ec7ed98a948ed51c210
Yi Zheng <847399102@qq.com>
2021-11-30 19:47:32 +0800

    finish totally of exit

--- game/urls/settings/index.py
+++ game/urls/settings/index.py
@@ -2,9 +2,11 @@ from django.urls import path
 from game.views.settings.getinfo import getinfo
 from game.views.settings.loginto import loginto
 from game.views.settings.reginto import reginto
+from game.views.settings.signout import signout
 
 urlpatterns = [
     path("getinfo/",getinfo,name="settings_getinfo"),
     path("loginto/",loginto,name="settings_loginto"),
     path("reginto/",reginto,name="settings_reginto"),
+    path("signout/",signout,name="settings_signout"),
 ]
