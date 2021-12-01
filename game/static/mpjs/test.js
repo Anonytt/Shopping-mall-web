1284fced991fb02ebcda1776fbeeaaf7e075ef20
Yi Zheng <847399102@qq.com>
2021-12-01 11:15:02 +0800

    left lunbo

--- game/static/mpjs/test.js
+++ game/static/mpjs/test.js
@@ -1,7 +1,27 @@
 $(function () {
+    var num = 0;
     $(".circle li").click(function () {
         $(this).addClass("current").siblings().removeClass("current");
         var index = $(this).index();
+        num = index;
         $(".photo li").eq(index).show().siblings().hide();
     });
+    $(".arrow-l").click(function () {
+        var max_len = 3;
+        num--;
+        if (num < 0) {
+            num = max_len;
+        }
+        $(".photo li").eq(num).show().siblings().hide();
+        $(".circle li").eq(num).addClass("current").siblings().removeClass("current");
+    })
+    $(".arrow-r").click(function () {
+        var max_len = 3;
+        num++;
+        if (num > max_len) {
+            num = 0;
+        }
+        $(".photo li").eq(num).show().siblings().hide();
+        $(".circle li").eq(num).addClass("current").siblings().removeClass("current");
+    })    
 });
