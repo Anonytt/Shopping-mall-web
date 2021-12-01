c389735123c761415db450759226ae8fda3c0b3e
Yi Zheng <847399102@qq.com>
2021-12-01 11:22:38 +0800

    finish lunbo

--- game/static/mpjs/test.js
+++ game/static/mpjs/test.js
@@ -23,5 +23,17 @@ $(function () {
         }
         $(".photo li").eq(num).show().siblings().hide();
         $(".circle li").eq(num).addClass("current").siblings().removeClass("current");
-    })    
+    })
+    function timer() {
+        time = setInterval(() => {
+            var max_len = 3;
+            num++;
+            if (num > max_len) {
+                num = 0;
+            }
+            $(".photo li").eq(num).show().siblings().hide();
+            $(".circle li").eq(num).addClass("current").siblings().removeClass("current");               
+        }, 1500);
+    }
+    timer();
 });
