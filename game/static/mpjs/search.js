4f90ac271804ecdb2c69b42a1da5e0c690d7cf75
Yi Zheng <847399102@qq.com>
2021-12-04 15:21:23 +0800

    finish searc

--- game/static/mpjs/search.js
+++ game/static/mpjs/search.js
@@ -16,5 +16,25 @@ $(function () {
                 }
             }
         });
-     })
+    });
+    $(document).keydown(function(event){
+        if(event.keyCode == 13){
+            var content = $(".search>input").val();
+            $.ajax({
+                url:"http://47.94.12.24:8000/settings/search",
+                type:"GET",
+                data:{
+                    content:content,
+                },
+                success:function(resp){
+                    if(resp.result === 'success'){
+                        self.location = resp.web;
+                    }
+                    else{
+                        alert("物品不存在");
+                    }
+                }
+            });
+        }
+    });
 })
